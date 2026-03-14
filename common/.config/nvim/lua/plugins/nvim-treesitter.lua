b6b0c140b2e594479d3fcce938de2379e2f4423e
Dio Luwandri <diovian.liu@gmail.com>
2026-03-14 14:08:41 +0800

    fix(treesitter): updated configs -> config

--- common/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ common/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,7 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs',
+  main = 'nvim-treesitter.config',
   opts = {
     ensure_installed = {
       'bash',
